7ffae0bb4edb51f867dcfa8327b3091b084a968c
Nazmul <nazmulislamn95@gmail.com>
2022-05-09 19:32:34 +0600

    Js Type Coversition

---
+++ style.js
@@ -0,0 +1,18 @@
+
+
+// const a = 10e6;
+// console.log(a.toExponential());
+
+// var  b = 10e6;
+// console.log(b.toFixed());
+
+// var c = 10.25585;
+// console.log(c.toPrecision());
+// console.log(b.toString());
+
+
+
+try{
+    console.log("This is try area");
+}
+console.error();
